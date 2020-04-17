a040c93e833052fae8f0574110b958243e1dd313
ngriffin26 <griffin.nikesha@gmail.com>
2020-04-17 06:07:57 -0700

    got data to display on the front-end

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,20 +1,38 @@
 import React, { Fragment, useEffect} from 'react'
-// import PropTypes from 'prop-types';
-// import { connect } from 'react-redux';
-// import Spinner from '../../layout/Spinner/Spinner';
-// import { getPosts } from '../../actions/post';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../../layout/Spinner/Spinner';
+import PostItem from '../Posts/PostItem';
+import { getPosts } from '../../actions/post';
 
 
-const Posts = props => {
-    return (
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+    useEffect(() => {
+        getPosts();
+    }, [getPosts]);
+    
+    return loading ? <Spinner /> : 
         <Fragment>
-            Discussions
-        </Fragment>
-    )
-}
+            <p className='lead'>
+            <i className='fas fa-user'></i> Welcome to CMUNITY
+            </p>
+            <h4 className='large text-primary'>Discussions</h4>
+            {/* PostForm */}
+            <div className='posts'>
+                {posts.map(post => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
+        </Fragment>;
+};
 
 Posts.propTypes = {
+    getPosts: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired
+};
 
-}
+const mapStateToProps = state => ({
+    post: state.post
+});
 
-export default Posts
+export default connect(mapStateToProps, { getPosts })(Posts);
