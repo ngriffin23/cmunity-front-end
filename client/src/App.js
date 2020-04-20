0246b496ba5f062f9e36bef4527bbdde0017dc69
ngriffin26 <griffin.nikesha@gmail.com>
2020-04-20 07:12:03 -0700

    added routes file

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './layout/Navbar.js/Navbar';
 import Landing from './layout/Landing.js/Landing';
-import Register from './auth/Register/Register';
-import Login from './auth/Login/Login';
-import Alert from './components/Alert';
-import Posts from './components/Posts/PostsContainer';
-import Post from './components/Post/Post';
-import PrivateRoute from './components/routing/PrivateRoute';
+import routes from './config/routes';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -29,16 +24,10 @@ const App = () => {
     <Provider store={store}>
       <Fragment>
         <Navbar/>
-        <Route exact path='/' component={Landing} />
-        <section className="container">
-          <Alert/>
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-            <PrivateRoute exact path='/posts' component={Posts} />
-            <PrivateRoute exact path='/posts/:id' component={Post} />
-          </Switch>
-        </section>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route component={routes} />
+        </Switch>
       </Fragment>
     </Provider>
   );
--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,2 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Register from '../auth/Register/Register';
+import Login from '../auth/Login/Login';
+import Alert from '../components/Alert';
+import Posts from '../components/Posts/PostsContainer';
+import Post from '../components/Post/Post';
+import PrivateRoute from '../components/routing/PrivateRoute';
 
+ const routes = () => {
+    return (
+        <section className="container">
+            <Alert/>
+            <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <PrivateRoute exact path='/posts' component={Posts} />
+            <PrivateRoute exact path='/posts/:id' component={Post} />
+            </Switch>
+    </section>
+    )
+};
+
+
+export default routes;
